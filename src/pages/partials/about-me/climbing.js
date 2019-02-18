43f36886cd33362bedc0a6eab08acaf02634e0fe
Hnilicka, Cormick <Cormick.Hnilicka@cdk.com>
2019-02-18 11:15:08 -0800

    first iteration of climbing

--- src/pages/partials/about-me/climbing.js
+++ src/pages/partials/about-me/climbing.js
@@ -3,10 +3,29 @@ import Gallery from 'react-grid-gallery';
 
 const Climbing = () => (
     <div>
-    <h1>I like Climbing</h1>
- 
+    <h1>Climbing</h1>
+    <p>
+        Climbing is a relatively new addition to my life as I have only been climbing for about a year, however, it still deserves a shout out when I try to present a holistic view on 'Me.' 
+        Like most other kids, I always enjoyed climbing trees, mounds of dirt, the house, etc. However, as I entered high school and focused on other sports,
+        my interest in ascending seemed to take a backseat. It was not until the later half of my time at college that climbing began to enter my life again. 
+    </p>
+    <p>
+        During my fourth year of college, I moved in with a buddy of mine who was really starting to get into climbing (more specifically boulding). He was the one 
+        that introduced me to a very special movie, Valley Uprising, that would become a very important part of my life. I didn't start to physically start climbing 
+        till about six months after college, but Valley Uprising would remain an escape for me during the stressful late nights of studying. For some reason, as long
+        as I had that movie playing, I was able to put all of the stress of work, school, and relationships behind me. 
+    </p>
+    <p>
+        Now fast forward a couple months after graduating. I am 'living' in my car traveling west to a new job and the next chapter in my life. While in Joshua tree,
+        I began to physically climbing the boulders and absolutely fell in love with it. The challenge that clmbing presented was addicting from the start, and all I
+        could think about was getting healthier and better so that I could climb better. And so began my love affair with climbing.
+    </p>
+    <p>
+        Much like Valley Uprising, climbing presents me with an environment where I can completely shut out stressers of my life and focus on the problem at hand. 
+        The puzzle-like nature of each route demands your attention to succeed. 
+    </p>
          {/* <Gallery enableImageSelection={false} images={IMAGES}/> */}
-         </div>
+    </div>
 );
 
 export default Climbing;
